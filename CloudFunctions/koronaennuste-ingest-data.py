77a0fe6fcaa12f1e66689bfdcbce0df9c662833f
Axel Johansson <axel.j.johansson@gmail.com>
2020-03-11 21:06:39 +0200

    update query

--- CloudFunctions/koronaennuste-ingest-data.py
+++ CloudFunctions/koronaennuste-ingest-data.py
@@ -20,7 +20,7 @@ def ingest_pubsub(event, context):
     from google.cloud import storage
 
     # get datetime
-    now = datetime.datetime.now() + datetime.timedelta(hours=6) # Just to make sure it goes to current day
+    now = datetime.datetime.now() #+ datetime.timedelta(hours=6) # Just to make sure it goes to current day
 
 
     # set storage client
@@ -29,21 +29,35 @@ def ingest_pubsub(event, context):
     # get bucket
     bucket = client2.get_bucket('koronaennuste-bucket') # without gs://
     confirmed_latest_file = bucket.blob('latest/confirmed.csv')
+    recovered_latest_file = bucket.blob('latest/recovered.csv')
+    deaths_latest_file = bucket.blob('latest/deaths.csv')
     daily_statistics_latest_file = bucket.blob('latest/daily_statistics.csv')
     confirmed_archive_file = bucket.blob(f"{now.year}/{now.month}/{now.day}/confirmed.csv")
+    recovered_archive_file = bucket.blob(f"{now.year}/{now.month}/{now.day}/recovered.csv")
+    deaths_archive_file = bucket.blob(f"{now.year}/{now.month}/{now.day}/deaths.csv")
     daily_statistics_archive_file = bucket.blob(f"{now.year}/{now.month}/{now.day}/daily_statistics.csv")
 
     # See if csv exists
-    if confirmed_archive_file.exists() == False and daily_statistics_archive_file.exists() == False:
+    if confirmed_archive_file.exists() == False and recovered_archive_file.exists() == False and deaths_archive_file.exists() == False and daily_statistics_archive_file.exists() == False:
         # copy file to google storage
         try:
             r = requests.get('https://w3qa5ydb4l.execute-api.eu-west-1.amazonaws.com/prod/finnishCoronaData')
             content = json.loads(r.content)
             confirmed = content['confirmed']
+            recovered = content['recovered']
+            deaths = content['deaths']
             df_confirmed = pd.DataFrame(confirmed)
+            df_recovered = pd.DataFrame(recovered)
+            df_deaths = pd.DataFrame(deaths)
+            csv_confirmed = df_confirmed.to_csv()
+            csv_recovered = df_recovered.to_csv()
+            csv_deaths = df_deaths.to_csv()
             for record in confirmed:
                 record['date'] = record['date'][:-14]
-
+            for record in recovered:
+                record['date'] = record['date'][:-14]
+            for record in deaths:
+                record['date'] = record['date'][:-14]
             ## Iterate over days
             # The size of each step in days
             day_delta = datetime.timedelta(days=1)
@@ -55,14 +69,28 @@ def ingest_pubsub(event, context):
             for i in range((end_date - start_date).days):
                 date = str(start_date + i*day_delta)
                 new_cases = sum(record['date'] == date for record in confirmed)
+                new_recoveries = sum(record['date'] == date for record in recovered)
+                new_deaths = sum(record['date'] == date for record in deaths)
                 if i == 0:
                     total_cases = new_cases + 1
+                    total_recoveries = new_recoveries + 1
+                    total_deaths = new_deaths + 0
                 else:
                     total_cases = daily_statistics[-1]['total_cases'] + new_cases
-                daily_statistics.append({'date': date, 'new_cases': new_cases, 'total_cases': total_cases, 'active_cases': total_cases - 1})
+                    total_recoveries = daily_statistics[-1]['total_recoveries'] + new_recoveries
+                    total_deaths = daily_statistics[-1]['total_deaths'] + new_deaths
+                daily_statistics.append({
+                    'date': date,
+                    'new_cases': new_cases,
+                    'total_cases': total_cases,
+                    'new_recoveries': new_recoveries,
+                    'total_recoveries': total_recoveries,
+                    'new_deaths': new_deaths,
+                    'total_deaths': total_deaths,
+                    'active_cases': total_cases - total_recoveries - total_deaths})
             df_daily_statistics = pd.DataFrame(daily_statistics)
-            csv_confirmed = df_confirmed.to_csv()
             csv_daily_statistics = df_daily_statistics.to_csv()
+            print(daily_statistics)
 
             ## Upload data
             confirmed_latest_file.upload_from_string(
@@ -70,6 +98,18 @@ def ingest_pubsub(event, context):
             )
             confirmed_archive_file.upload_from_string(
                 data = csv_confirmed
+            )
+            recovered_latest_file.upload_from_string(
+                data = csv_recovered
+            )
+            recovered_archive_file.upload_from_string(
+                data = csv_recovered
+            )
+            deaths_latest_file.upload_from_string(
+                data = csv_deaths
+            )
+            deaths_archive_file.upload_from_string(
+                data = csv_deaths
             )
             daily_statistics_latest_file.upload_from_string(
                 data = csv_daily_statistics
@@ -82,3 +122,6 @@ def ingest_pubsub(event, context):
             raise
     else:
         print('archive_file already exists in google storage')
+
+if __name__ == "__main__":
+    ingest_pubsub(None, None)
